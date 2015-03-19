37b6850ee90577bcbcb2342c535d3981f67b8dc4
Yura95st <yura95st@gmail.com>
2015-03-19 13:56:46 +0200

    Modify Main class: re-factor and add printing rightRecursive rules. Modify grammar file: add simple rightRecursive rule.

--- src/grammar_parser/Main.java
+++ src/grammar_parser/Main.java
@@ -1,12 +1,5 @@
 package grammar_parser;
 
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Map;
-
 import grammar_parser.Enums.SpecialNodeKind;
 import grammar_parser.Helpers.ExceptionHelper;
 import grammar_parser.Lexers.Abstract.IGrammarLexer;
@@ -16,81 +9,21 @@ import grammar_parser.Models.Node;
 import grammar_parser.Models.Rule;
 import grammar_parser.Parsers.Abstract.IGrammarParser;
 import grammar_parser.Parsers.Concrete.GrammarParser;
-import grammar_parser.Utils.Guard;
+import grammar_parser.Services.Abstract.IGrammarService;
+import grammar_parser.Services.Concrete.GrammarService;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Main
 {
-	public static String getStringFromGrammar(Grammar grammar,
-		Map<SpecialNodeKind, Node> specialNodesDictionary)
-	{
-		Guard.notNull(grammar, "grammar");
-		Guard.notNull(specialNodesDictionary, "specialNodesDictionary");
-
-		StringBuilder stringBuilder = new StringBuilder();
-
-		for (List<Rule> rules : grammar.getRulesDictionary().values())
-		{
-			for (Rule rule : rules)
-			{
-				stringBuilder.append(rule.getHeadNode().getText());
-				stringBuilder.append(" ");
-				stringBuilder.append(specialNodesDictionary.get(
-					SpecialNodeKind.Definition).getText());
-
-				boolean isFirstNode = true;
-
-				for (Node node : rule.getNodes())
-				{
-					if (!isFirstNode)
-					{
-						stringBuilder.append(specialNodesDictionary.get(
-							SpecialNodeKind.Concatenation).getText());
-					}
-					else
-					{
-						isFirstNode = false;
-					}
-
-					stringBuilder.append(" ");
-					stringBuilder.append(node.getText());
-				}
-
-				stringBuilder.append(" ");
-				stringBuilder.append(specialNodesDictionary.get(
-					SpecialNodeKind.Termination).getText());
-
-				stringBuilder.append(System.lineSeparator());
-				stringBuilder.append(System.lineSeparator());
-			}
-		}
-
-		String string = stringBuilder.toString();
-
-		return string;
-	}
-
-	public static String getStringFromNodes(List<Node> nodes)
-	{
-		Guard.notNull(nodes, "nodes");
-
-		StringBuilder stringBuilder = new StringBuilder();
-
-		for (Node node : nodes)
-		{
-			stringBuilder.append(String.format("%1$s (%2$s)", node.getText(),
-				node.getKind().name()));
-			stringBuilder.append(System.lineSeparator());
-		}
-
-		String result = stringBuilder.toString();
-
-		return result;
-	}
-
 	public static void main(String[] args)
 	{
-		IGrammarLexer grammarLexer = new GrammarLexer();
-
 		try
 		{
 			Path path = Paths.get(args[0]);
@@ -108,13 +41,19 @@ public class Main
 
 			String source = stringBuilder.toString().trim();
 
+			IGrammarLexer grammarLexer = new GrammarLexer();
+
 			grammarLexer.setSource(source);
 
 			List<Node> nodes = grammarLexer.parse();
 
-			System.out.println("----- Nodes: -----");
-			System.out.println();
-			System.out.println(Main.getStringFromNodes(nodes));
+			System.out.println(String.format("----- Nodes: -----%1$s",
+				System.getProperty("line.separator")));
+
+			for (Node node : nodes)
+			{
+				System.out.println(Main.nodeToString(node));
+			}
 
 			IGrammarParser grammarParser = new GrammarParser();
 
@@ -122,16 +61,98 @@ public class Main
 
 			Grammar grammar = grammarParser.parse();
 
-			System.out.println("----- Grammar: -----");
-			System.out.println();
-			System.out.println(Main.getStringFromGrammar(grammar,
-				grammarParser.getSpecialNodesDictionary()));
+			System.out.println(String.format("%1$s----- Grammar: -----%1$s",
+				System.getProperty("line.separator")));
+
+			Map<SpecialNodeKind, Node> specialNodesDictionary = grammarParser
+					.getSpecialNodesDictionary();
+
+			for (List<Rule> rules : grammar.getRulesDictionary().values())
+			{
+				for (Rule rule : rules)
+				{
+					System.out.println(Main.ruleToString(rule,
+						specialNodesDictionary));
+				}
+			}
+
+			IGrammarService grammarService = new GrammarService();
+
+			System.out.println(String.format(
+				"%1$s----- RightRecursive rules: -----%1$s",
+				System.getProperty("line.separator")));
+
+			Set<Rule> rightRecursiveRules = grammarService
+					.getRightRecursiveRules(grammar);
+
+			if (rightRecursiveRules.size() == 0)
+			{
+				System.out
+				.println("Grammar doesn't contain any rightRecursive rule.");
+			}
+			else
+			{
+				for (Rule rule : rightRecursiveRules)
+				{
+					System.out.println(Main.ruleToString(rule,
+						specialNodesDictionary));
+				}
+			}
 		}
 		catch (Exception exception)
 		{
 			System.out.println("Error occured:");
 			System.out.println(ExceptionHelper
-				.getFullExceptionMessage(exception));
+					.getFullExceptionMessage(exception));
 		}
+	}
+
+	static String nodeToString(Node node)
+	{
+		StringBuilder stringBuilder = new StringBuilder();
+
+		stringBuilder.append(String.format("%1$s (%2$s)", node.getText(), node
+				.getKind().name()));
+
+		String resultString = stringBuilder.toString();
+
+		return resultString;
+	}
+
+	static String ruleToString(Rule rule,
+		Map<SpecialNodeKind, Node> specialNodesDictionary)
+	{
+		StringBuilder stringBuilder = new StringBuilder();
+
+		stringBuilder.append(rule.getHeadNode().getText());
+		stringBuilder.append(" ");
+		stringBuilder.append(specialNodesDictionary.get(
+			SpecialNodeKind.Definition).getText());
+
+		boolean isFirstNode = true;
+
+		for (Node node : rule.getNodes())
+		{
+			if (!isFirstNode)
+			{
+				stringBuilder.append(specialNodesDictionary.get(
+					SpecialNodeKind.Concatenation).getText());
+			}
+			else
+			{
+				isFirstNode = false;
+			}
+
+			stringBuilder.append(" ");
+			stringBuilder.append(node.getText());
+		}
+
+		stringBuilder.append(" ");
+		stringBuilder.append(specialNodesDictionary.get(
+			SpecialNodeKind.Termination).getText());
+
+		String resultString = stringBuilder.toString();
+
+		return resultString;
 	}
 }
