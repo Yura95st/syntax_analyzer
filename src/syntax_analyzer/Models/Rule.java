a68adbd8d8de89fefc2f072a7946a1c1a437ea9f
Yura95st <yura95st@gmail.com>
2015-03-19 13:36:28 +0200

    Modify Node and Rule model classes: add "equals" and "getHashCode" methods.

--- src/syntax_analyzer/Models/Rule.java
+++ src/syntax_analyzer/Models/Rule.java
@@ -20,6 +20,53 @@ public class Rule
 		this._nodes = new ArrayList<Node>();
 	}
 	
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+		{
+			return true;
+		}
+
+		if (obj == null)
+		{
+			return false;
+		}
+
+		if (this.getClass() != obj.getClass())
+		{
+			return false;
+		}
+
+		Rule other = (Rule) obj;
+
+		if (this._head == null)
+		{
+			if (other._head != null)
+			{
+				return false;
+			}
+		}
+		else if (!this._head.equals(other._head))
+		{
+			return false;
+		}
+
+		if (this._nodes == null)
+		{
+			if (other._nodes != null)
+			{
+				return false;
+			}
+		}
+		else if (!this._nodes.equals(other._nodes))
+		{
+			return false;
+		}
+
+		return true;
+	}
+
 	public Node getHead()
 	{
 		return this._head;
@@ -28,6 +75,21 @@ public class Rule
 	public List<Node> getNodes()
 	{
 		return new ArrayList<Node>(this._nodes);
+	}
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+
+		result = prime * result
+				+ ((this._head == null) ? 0 : this._head.hashCode());
+
+		result = prime * result
+				+ ((this._nodes == null) ? 0 : this._nodes.hashCode());
+
+		return result;
 	}
 
 	public void setNodes(List<Node> nodes)
