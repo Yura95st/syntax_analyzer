7db704bb27108cb25998e51eed40143140169ffa
Yura95st <yura95st@gmail.com>
2015-03-30 21:40:10 +0300

    Modify GrammarParserTests: add test "Rule must start from nonterminalNode".

--- grammar_parser/test/grammar_parser/Parsers/Concrete/GrammarParserTests.java
+++ grammar_parser/test/grammar_parser/Parsers/Concrete/GrammarParserTests.java
@@ -1,14 +1,5 @@
 package grammar_parser.Parsers.Concrete;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
 import grammar_parser.Enums.NodeKind;
 import grammar_parser.Enums.SpecialNodeKind;
 import grammar_parser.Exceptions.InvalidGrammarException;
@@ -18,29 +9,61 @@ import grammar_parser.Models.Node;
 import grammar_parser.Models.Rule;
 import grammar_parser.Parsers.Abstract.IGrammarParser;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class GrammarParserTests
 {
 	private Map<SpecialNodeKind, Node> _specialNodesDictionary;
 
 	IGrammarParser _grammarParser;
 
+	@Test(expected = InvalidGrammarException.class)
+	public void parse_GrammarRuleDoesNotStartFromNonterminalNode_ThrowsInvalidGrammarException()
+		throws Exception
+	{
+		// Arrange - create nodes
+		Node nodeA = new Node(NodeKind.Terminal, "a");
+		Node nodeB = new Node(NodeKind.Nonterminal, "B");
+
+		Node[] nodes =
+			{
+				nodeA,
+				this._specialNodesDictionary.get(SpecialNodeKind.Definition),
+				nodeB,
+				this._specialNodesDictionary.get(SpecialNodeKind.Termination)
+			}; // "a" = B .
+
+		this._grammarParser.setNodes(Arrays.asList(nodes));
+
+		// Act & Assert
+		this._grammarParser.parse();
+	}
+
 	@Test
 	public void parse_GrammarRuleWithAlternatives_SplitsAlternativesToDifferentRules()
-			throws Exception
+		throws Exception
 	{
 		// Arrange - create nodes
 		Node nodeA = new Node(NodeKind.Nonterminal, "A");
 		Node nodeB = new Node(NodeKind.Nonterminal, "B");
 		Node nodeC = new Node(NodeKind.Terminal, "c");
 
-		Node[] nodes = {
-			nodeA,
-			this._specialNodesDictionary.get(SpecialNodeKind.Definition),
-			nodeB,
-			this._specialNodesDictionary.get(SpecialNodeKind.Alternation),
-			nodeC,
-			this._specialNodesDictionary.get(SpecialNodeKind.Termination),
-		}; // A = B | "c" .
+		Node[] nodes =
+			{
+				nodeA,
+				this._specialNodesDictionary.get(SpecialNodeKind.Definition),
+				nodeB,
+				this._specialNodesDictionary.get(SpecialNodeKind.Alternation),
+				nodeC,
+				this._specialNodesDictionary.get(SpecialNodeKind.Termination),
+			}; // A = B | "c" .
 
 		// Arrange - create rules
 		Rule ruleOne = new Rule(nodeA);
@@ -70,18 +93,19 @@ public class GrammarParserTests
 
 	@Test(expected = InvalidGrammarException.class)
 	public void parse_GrammarRuleWithInvalidAlternationSpecialNode_ThrowsInvalidGrammarException()
-			throws Exception
+		throws Exception
 	{
 		// Arrange - create nodes
 		Node nodeA = new Node(NodeKind.Nonterminal, "A");
 
-		Node[] nodes = {
-			nodeA,
-			this._specialNodesDictionary.get(SpecialNodeKind.Definition),
-			this._specialNodesDictionary.get(SpecialNodeKind.Alternation),
-			this._specialNodesDictionary.get(SpecialNodeKind.Termination)
+		Node[] nodes =
+			{
+				nodeA,
+				this._specialNodesDictionary.get(SpecialNodeKind.Definition),
+				this._specialNodesDictionary.get(SpecialNodeKind.Alternation),
+				this._specialNodesDictionary.get(SpecialNodeKind.Termination)
 
-		}; // A = | .
+			}; // A = | .
 
 		this._grammarParser.setNodes(Arrays.asList(nodes));
 
@@ -91,21 +115,22 @@ public class GrammarParserTests
 
 	@Test(expected = InvalidGrammarException.class)
 	public void parse_GrammarRuleWithInvalidConcatenationSpecialNode_ThrowsInvalidGrammarException()
-			throws Exception
+		throws Exception
 	{
 		// Arrange - create nodes
 		Node nodeA = new Node(NodeKind.Nonterminal, "A");
 		Node nodeB = new Node(NodeKind.Nonterminal, "B");
 
-		Node[] nodes = {
-			nodeA,
-			this._specialNodesDictionary.get(SpecialNodeKind.Definition),
-			nodeB,
-			this._specialNodesDictionary.get(SpecialNodeKind.Concatenation),
-			this._specialNodesDictionary.get(SpecialNodeKind.Alternation),
-			this._specialNodesDictionary.get(SpecialNodeKind.Termination)
+		Node[] nodes =
+			{
+				nodeA,
+				this._specialNodesDictionary.get(SpecialNodeKind.Definition),
+				nodeB,
+				this._specialNodesDictionary.get(SpecialNodeKind.Concatenation),
+				this._specialNodesDictionary.get(SpecialNodeKind.Alternation),
+				this._specialNodesDictionary.get(SpecialNodeKind.Termination)
 
-		}; // A = B , | .
+			}; // A = B , | .
 
 		this._grammarParser.setNodes(Arrays.asList(nodes));
 
@@ -115,20 +140,21 @@ public class GrammarParserTests
 
 	@Test(expected = InvalidGrammarException.class)
 	public void parse_GrammarRuleWithMissingConcatenationOrAlternationSpecialNode_ThrowsInvalidGrammarException()
-			throws Exception
+		throws Exception
 	{
 		// Arrange - create nodes
 		Node nodeA = new Node(NodeKind.Nonterminal, "A");
 		Node nodeB = new Node(NodeKind.Nonterminal, "B");
 		Node nodeC = new Node(NodeKind.Terminal, "c");
 
-		Node[] nodes = {
-			nodeA,
-			this._specialNodesDictionary.get(SpecialNodeKind.Definition),
-			nodeB, nodeC,
-			this._specialNodesDictionary.get(SpecialNodeKind.Termination)
+		Node[] nodes =
+			{
+				nodeA,
+				this._specialNodesDictionary.get(SpecialNodeKind.Definition),
+				nodeB, nodeC,
+				this._specialNodesDictionary.get(SpecialNodeKind.Termination)
 
-		}; // A = B "c" .
+			}; // A = B "c" .
 
 		this._grammarParser.setNodes(Arrays.asList(nodes));
 
@@ -138,15 +164,16 @@ public class GrammarParserTests
 
 	@Test(expected = InvalidGrammarException.class)
 	public void parse_GrammarRuleWithMissingDefinitionSpecialNode_ThrowsInvalidGrammarException()
-			throws Exception
+		throws Exception
 	{
 		// Arrange - create nodes
 		Node nodeA = new Node(NodeKind.Nonterminal, "A");
 
-		Node[] nodes = {
-			nodeA,
-			this._specialNodesDictionary.get(SpecialNodeKind.Termination),
-		}; // A .
+		Node[] nodes =
+			{
+				nodeA,
+				this._specialNodesDictionary.get(SpecialNodeKind.Termination),
+			}; // A .
 
 		this._grammarParser.setNodes(Arrays.asList(nodes));
 
@@ -156,14 +183,14 @@ public class GrammarParserTests
 
 	@Test(expected = InvalidGrammarException.class)
 	public void parse_GrammarRuleWithMissingTerminationSpecialNode_ThrowsInvalidGrammarException()
-			throws Exception
+		throws Exception
 	{
 		// Arrange - create nodes
 		Node nodeA = new Node(NodeKind.Nonterminal, "A");
 
 		Node[] nodes = {
 			nodeA, this._specialNodesDictionary.get(SpecialNodeKind.Definition)
-		}; // A = B
+		}; // A =
 
 		this._grammarParser.setNodes(Arrays.asList(nodes));
 
@@ -173,20 +200,21 @@ public class GrammarParserTests
 
 	@Test
 	public void parse_GrammarWithDuplicateRules_RemoveDuplicateRules()
-		throws Exception
+			throws Exception
 	{
 		// Arrange - create nodes
 		Node nodeA = new Node(NodeKind.Nonterminal, "A");
 		Node nodeB = new Node(NodeKind.Nonterminal, "B");
 
-		Node[] nodes = {
-			nodeA,
-			this._specialNodesDictionary.get(SpecialNodeKind.Definition),
-			nodeB,
-			this._specialNodesDictionary.get(SpecialNodeKind.Alternation),
-			nodeB,
-			this._specialNodesDictionary.get(SpecialNodeKind.Termination),
-		}; // A = B | B .
+		Node[] nodes =
+			{
+				nodeA,
+				this._specialNodesDictionary.get(SpecialNodeKind.Definition),
+				nodeB,
+				this._specialNodesDictionary.get(SpecialNodeKind.Alternation),
+				nodeB,
+				this._specialNodesDictionary.get(SpecialNodeKind.Termination),
+			}; // A = B | B .
 
 		// Arrange - create rule
 		Rule rule = new Rule(nodeA);
@@ -211,16 +239,17 @@ public class GrammarParserTests
 
 	@Test
 	public void parse_GrammarWithEmptyRule_ReturnsValidGrammar()
-			throws Exception
+		throws Exception
 	{
 		// Arrange - create nodes
 		Node nodeA = new Node(NodeKind.Nonterminal, "A");
 
-		Node[] nodes = {
-			nodeA,
-			this._specialNodesDictionary.get(SpecialNodeKind.Definition),
-			this._specialNodesDictionary.get(SpecialNodeKind.Termination),
-		}; // A = .
+		Node[] nodes =
+			{
+				nodeA,
+				this._specialNodesDictionary.get(SpecialNodeKind.Definition),
+				this._specialNodesDictionary.get(SpecialNodeKind.Termination),
+			}; // A = .
 
 		// Arrange - create rule
 		Rule rule = new Rule(nodeA);
@@ -242,7 +271,7 @@ public class GrammarParserTests
 
 	@Test
 	public void parse_GrammarWithSimpleRules_ReturnsValidGrammar()
-			throws Exception
+		throws Exception
 	{
 		// Arrange - create nodes
 		Node nodeA = new Node(NodeKind.Nonterminal, "A");
@@ -250,18 +279,19 @@ public class GrammarParserTests
 		Node nodeC = new Node(NodeKind.Terminal, "c");
 		Node nodeD = new Node(NodeKind.Terminal, "d");
 
-		Node[] nodes = {
-			nodeA,
-			this._specialNodesDictionary.get(SpecialNodeKind.Definition),
-			nodeB,
-			this._specialNodesDictionary.get(SpecialNodeKind.Concatenation),
-			nodeC,
-			this._specialNodesDictionary.get(SpecialNodeKind.Termination),
-			nodeB,
-			this._specialNodesDictionary.get(SpecialNodeKind.Definition),
-			nodeD,
-			this._specialNodesDictionary.get(SpecialNodeKind.Termination)
-		}; // A = B , "c" . B = "d" .
+		Node[] nodes =
+			{
+				nodeA,
+				this._specialNodesDictionary.get(SpecialNodeKind.Definition),
+				nodeB,
+				this._specialNodesDictionary.get(SpecialNodeKind.Concatenation),
+				nodeC,
+				this._specialNodesDictionary.get(SpecialNodeKind.Termination),
+				nodeB,
+				this._specialNodesDictionary.get(SpecialNodeKind.Definition),
+				nodeD,
+				this._specialNodesDictionary.get(SpecialNodeKind.Termination)
+			}; // A = B , "c" . B = "d" .
 
 		// Arrange - create rules
 		Rule ruleOne = new Rule(nodeA);
@@ -293,12 +323,13 @@ public class GrammarParserTests
 	public void parse_MethodIsIdempotent() throws Exception
 	{
 		// Arrange
-		Node[] nodes = {
-			new Node(NodeKind.Nonterminal, "A"),
-			this._specialNodesDictionary.get(SpecialNodeKind.Definition),
-			new Node(NodeKind.Nonterminal, "B"),
-			this._specialNodesDictionary.get(SpecialNodeKind.Termination),
-		}; // A = B .
+		Node[] nodes =
+			{
+				new Node(NodeKind.Nonterminal, "A"),
+				this._specialNodesDictionary.get(SpecialNodeKind.Definition),
+				new Node(NodeKind.Nonterminal, "B"),
+				this._specialNodesDictionary.get(SpecialNodeKind.Termination),
+			}; // A = B .
 
 		this._grammarParser.setNodes(Arrays.asList(nodes));
 
@@ -326,22 +357,23 @@ public class GrammarParserTests
 
 	@Test(expected = SpecialNodeIsNotDefinedException.class)
 	public void setSpecialNodesDictionary_NotAllSpecialNodesAreDefined_ThrowsSpecialNodeIsNotDefinedException()
-			throws Exception
+		throws Exception
 	{
 		// Arrange
-		Map<SpecialNodeKind, Node> specialNodesDictionary = new HashMap<SpecialNodeKind, Node>() {
-			{
-				this.put(SpecialNodeKind.Alternation, new Node(
-					NodeKind.Delimiter, "node"));
+		Map<SpecialNodeKind, Node> specialNodesDictionary =
+			new HashMap<SpecialNodeKind, Node>() {
+				{
+					this.put(SpecialNodeKind.Alternation, new Node(
+						NodeKind.Delimiter, "node"));
 
-				this.put(SpecialNodeKind.Concatenation, new Node(
-					NodeKind.Delimiter, "node"));
+					this.put(SpecialNodeKind.Concatenation, new Node(
+						NodeKind.Delimiter, "node"));
 
-				this.put(SpecialNodeKind.Definition, null);
+					this.put(SpecialNodeKind.Definition, null);
 
-				this.put(SpecialNodeKind.Termination, null);
-			}
-		};
+					this.put(SpecialNodeKind.Termination, null);
+				}
+			};
 
 		// Act & Assert
 		this._grammarParser.setSpecialNodesDictionary(specialNodesDictionary);
@@ -352,8 +384,8 @@ public class GrammarParserTests
 	{
 		this._grammarParser = new GrammarParser();
 
-		this._specialNodesDictionary = this._grammarParser
-				.getSpecialNodesDictionary();
+		this._specialNodesDictionary =
+			this._grammarParser.getSpecialNodesDictionary();
 	}
 
 }
